69d0d6beb9dcdeb14915decc65568dae3aeb1374
Yuki Nishina <project_flower@outlook.com>
2021-05-01 19:36:24 +0900

    新規作成

---
+++ CurrentPaper/FormMain.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace CurrentPaper
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelWallpaper = new System.Windows.Forms.Label();
+            this.listBoxPaths = new System.Windows.Forms.ListBox();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonExplore = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelWallpaper
+            // 
+            this.labelWallpaper.AutoSize = true;
+            this.labelWallpaper.Location = new System.Drawing.Point(12, 9);
+            this.labelWallpaper.Name = "labelWallpaper";
+            this.labelWallpaper.Size = new System.Drawing.Size(56, 12);
+            this.labelWallpaper.TabIndex = 0;
+            this.labelWallpaper.Text = "&Wallpaper:";
+            // 
+            // listBoxPaths
+            // 
+            this.listBoxPaths.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxPaths.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.listBoxPaths.FormattingEnabled = true;
+            this.listBoxPaths.HorizontalScrollbar = true;
+            this.listBoxPaths.ItemHeight = 12;
+            this.listBoxPaths.Location = new System.Drawing.Point(12, 24);
+            this.listBoxPaths.Name = "listBoxPaths";
+            this.listBoxPaths.Size = new System.Drawing.Size(695, 376);
+            this.listBoxPaths.TabIndex = 1;
+            this.listBoxPaths.DoubleClick += new System.EventHandler(this.listBoxPaths_DoubleClick);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRefresh.Location = new System.Drawing.Point(713, 24);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 2;
+            this.buttonRefresh.Text = "&Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpen.Location = new System.Drawing.Point(632, 415);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 3;
+            this.buttonOpen.Text = "&Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonExplore
+            // 
+            this.buttonExplore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExplore.Location = new System.Drawing.Point(713, 415);
+            this.buttonExplore.Name = "buttonExplore";
+            this.buttonExplore.Size = new System.Drawing.Size(75, 23);
+            this.buttonExplore.TabIndex = 4;
+            this.buttonExplore.Text = "&Explore";
+            this.buttonExplore.UseVisualStyleBackColor = true;
+            this.buttonExplore.Click += new System.EventHandler(this.buttonExplore_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonExplore);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.listBoxPaths);
+            this.Controls.Add(this.labelWallpaper);
+            this.Name = "FormMain";
+            this.Text = "CurrentPaper";
+            this.Load += new System.EventHandler(this.load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelWallpaper;
+        private System.Windows.Forms.ListBox listBoxPaths;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonExplore;
+    }
+}
+
---
+++ CurrentPaper/MainEngine.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrentPaper
+{
+    public static class MainEngine
+    {
+        public static string[] GetCurrentWallpapers()
+        {
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop"))
+                {
+                    int count = (int)key.GetValue("TranscodedImageCount");
+                    string valueName = "TranscodedImageCache";
+                    var resultList = new List<string>();
+
+                    if (count < 2)
+                    {
+                        AddTranscodedImageCache(key, valueName, resultList);
+                    }
+                    else
+                    {
+                        for (int i = 0; i < count; ++i)
+                        {
+                            AddTranscodedImageCache(key, string.Format("{0}_{1:D3}", valueName, i), resultList);
+                        }
+                    }
+
+                    return resultList.ToArray();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private static void AddTranscodedImageCache(RegistryKey key, string valueName, List<string> list)
+        {
+            byte[] bytes;
+
+            try
+            {
+                bytes = key.GetValue(valueName) as byte[];
+            }
+            catch
+            {
+                throw;
+            }
+
+            string text;
+
+            try
+            {
+                text = Encoding.Unicode.GetString(bytes);
+            }
+            catch
+            {
+                list.Add("???");
+                return;
+            }
+
+            int startIndex;
+            bool found = false;
+
+            for (startIndex = 0; startIndex < text.Length; ++startIndex)
+            {
+                char c = text[startIndex];
+
+                if ((c < 'A') || (c > 'z'))
+                {
+                    continue;
+                }
+
+                found = true;
+                break;
+            }
+
+            string value;
+
+            if (!found)
+            {
+                value = "???";
+            }
+            else
+            {
+                try
+                {
+                    value = text.Substring(startIndex).Trim();
+                }
+                catch
+                {
+                    value = "???";
+                }
+            }
+
+            list.Add(value);
+        }
+    }
+}
